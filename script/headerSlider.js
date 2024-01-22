8f6d231ebebfa452b107b2634d7fdbaaaae30c7b
Kirill Pekarskiy <kipe122006@gmail.com>
2024-01-22 18:31:50 +0300

    Изменение функционала слайдера и исправление багов, связанных со слайдером

--- script/headerSlider.js
+++ script/headerSlider.js
@@ -1,42 +1,44 @@
-const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
-    loop:true,
-    slidesPerView: 2,
-    spaceBetween: 25,
-    navigation: {
-      nextEl: '.btn-swiper-next',
-      prevEl: '.btn-swiper-prev',
-    },
-});
-
 let next = document.querySelector(".btn-swiper-next");
 let prev = document.querySelector(".btn-swiper-prev");
 let slides = document.querySelectorAll(".swiper-slide");
 let header = document.querySelector(".header");
 
-next.addEventListener("click", function() {
-
-    slides.forEach(slide => {
-        if (slide.classList.contains("swiper-slide-active")) {
-            
-            let slideContent = slide.firstElementChild;
-            let imgPath = window.getComputedStyle(slideContent).backgroundImage;
-
-            header.style.backgroundImage = imgPath;
-        }
-    })
-})
-
-prev.addEventListener("click", function() {
-
-    slides.forEach(slide => {
-        if (slide.classList.contains("swiper-slide-next")) {
-            
-            let slideContent = slide.firstElementChild;
-            let imgPath = window.getComputedStyle(slideContent).backgroundImage;
-
-            header.style.backgroundImage = imgPath;
-        }
-    })
-})
+const swiper = new Swiper('.swiper', {
+    speed: 1000,
+    spaceBetween: 100,
+    loop:true,
+    slidesPerView: 2,
+    spaceBetween: 25,
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.btn-swiper-next',
+      prevEl: '.btn-swiper-prev',
+    },
+    on: {
+        slideNextTransitionStart: function (swiper) {
+            slides.forEach(slide => {
+                if (slide.classList.contains("swiper-slide-prev")) {
+                    
+                    let slideContent = slide.firstElementChild;
+                    let imgPath = window.getComputedStyle(slideContent).backgroundImage;
+        
+                    header.style.backgroundImage = imgPath;
+                }
+            })
+        },
+        slidePrevTransitionStart: function (swiper) {
+            slides.forEach(slide => {
+                if (!slide.classList.contains("swiper-slide-active") && !slide.classList.contains("swiper-slide-next")) {
+                    
+                    let slideContent = slide.firstElementChild;
+                    let imgPath = window.getComputedStyle(slideContent).backgroundImage;
+        
+                    header.style.backgroundImage = imgPath;
+                }
+            })
+        },
+    }
+});
