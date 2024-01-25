0649f99f0041123d2f8be1a6822aad02c249a45d
Kirill Pekarskiy <kipe122006@gmail.com>
2024-01-25 21:50:00 +0300

    Написание фунционала скрытия и появления скролла

---
+++ script/closeAndOpenScroll.js
@@ -0,0 +1,53 @@
+// Работа со скроллом при скрытии и появлении формы
+function scroll() {
+    let btnRequests = document.querySelectorAll(".btn-request");
+    let body = document.querySelector("body");
+    let lockPadding = document.querySelectorAll(".lock-padding");
+    let overblock = document.querySelector(".overblock");
+
+    let timeout = 500;
+
+    if (btnRequests.length > 0) {
+        btnRequests.forEach(btnRequest => {
+            btnRequest.addEventListener('click', function() {
+                bodyLock();
+            })
+        })
+    }
+    let formClosebtns = document.querySelectorAll(".popup__btn-close");
+    if (formClosebtns.length > 0) {
+        formClosebtns.forEach(formClosebtn => {
+            formClosebtn.addEventListener("click", function() {
+                bodyunLock();
+            })
+        })
+    }
+    overblock.addEventListener("click", function() {
+        bodyunLock();
+    })
+    function bodyLock(){
+        const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
+        
+        if (lockPadding > 0) {
+            lockPadding.forEach(el => {
+                el.style.paddingRight = lockPaddingValue;
+            })
+        }
+
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add("lock");
+    }
+    function bodyunLock() {
+        setTimeout(function() {
+            if (lockPadding > 0) {
+                lockPadding.forEach(el => {
+                    el.style.paddingRight = 0;
+                })
+            }
+
+            body.style.paddingRight = 0;
+            body.classList.remove("lock");
+        },timeout);
+    }
+}
+scroll()
--- script/formAppearAndHide.js
+++ script/formAppearAndHide.js
@@ -1,52 +1,64 @@
-let btnRequests = document.querySelectorAll(".btn-request");
+// Скрытие и появление формы
+function openHideForms() {
 
-btnRequests.forEach(btnRequest => {
-    btnRequest.addEventListener("click", function() {
+    let btnRequests = document.querySelectorAll(".btn-request");
+    let overblock = document.querySelector(".overblock");
 
-        if (this.dataset.request) {
-            let data = this.dataset.request
-            console.log(data)
-            
-            let form = document.querySelector(`${data}`)
+    btnRequests.forEach(btnRequest => {
+        btnRequest.addEventListener("click", function() {
 
-            if (form) {
-                form.classList.toggle("popup_act")
+            if (this.dataset.request) {
+                let data = this.dataset.request;
 
-                let btnClose = form.querySelector(".popup__btn-close");
-                btnClose.addEventListener("click", function() {
-                    form.classList.remove("popup_act");
-                })
+                let form = document.querySelector(`${data}`);
+                overblock.classList.add("overblock_act");
 
-                let btnSlide = form.querySelector(".popup__btn-slide");
-                let bodyPrev;
-                let bodyNext;
-                btnSlide.addEventListener("click", function() {
-                    bodyPrev = form.querySelector('.popup__body-prev');
-                    bodyNext = form.querySelector('.popup__body-next');
+                if (form) {
+                    form.classList.toggle("popup_act");
 
-                    if (bodyPrev.classList.contains('open')) {
-                        prevStep()
-                        setTimeout(function() {
-                            bodyPrev.classList.remove("open")
-                        },0)
-                    } else {
-                        nextStep()
-                        setTimeout(function() {
-                            bodyPrev.classList.add("open")
-                        },0)
+                    let btnClose = form.querySelector(".popup__btn-close");
+                    btnClose.addEventListener("click", function() {
+                        form.classList.remove("popup_act");
+                        overblock.classList.remove("overblock_act");
+                    })
+
+                    overblock.addEventListener("click", function() {
+                        form.classList.remove("popup_act");
+                        overblock.classList.remove("overblock_act");
+                    })
+
+                    let btnSlide = form.querySelector(".popup__btn-slide");
+                    let bodyPrev;
+                    let bodyNext;
+                    btnSlide.addEventListener("click", function() {
+                        bodyPrev = form.querySelector('.popup__body-prev');
+                        bodyNext = form.querySelector('.popup__body-next');
+
+                        if (bodyPrev.classList.contains('open')) {
+                            prevStep()
+                            setTimeout(function() {
+                                bodyPrev.classList.remove("open")
+                            },0)
+                        } else {
+                            nextStep()
+                            setTimeout(function() {
+                                bodyPrev.classList.add("open")
+                            },0)
+                        }
+                    })
+                    function nextStep() {
+                        bodyPrev.classList.remove("popup__body-prev_act")
+                        bodyNext.classList.remove("popup__body-next_act")
+                        btnSlide.textContent = "Следующий шаг"
                     }
-                })
-                function nextStep() {
-                    bodyPrev.classList.remove("popup__body-prev_act")
-                    bodyNext.classList.remove("popup__body-next_act")
-                    btnSlide.textContent = "Следующий шаг"
-                }
-                function prevStep() {
-                    bodyPrev.classList.add("popup__body-prev_act")
-                    bodyNext.classList.add("popup__body-next_act")
-                    btnSlide.textContent = "Предыдущий шаг"
+                    function prevStep() {
+                        bodyPrev.classList.add("popup__body-prev_act")
+                        bodyNext.classList.add("popup__body-next_act")
+                        btnSlide.textContent = "Предыдущий шаг"
+                    }
                 }
             }
-        }
+        })
     })
-})
+}
+openHideForms()
